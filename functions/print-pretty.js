bab09d7e6bd253128ebbe76407ea7d20124144da
caggles <caggles@users.noreply.github.com>
2020-03-05 18:19:13 -0800

    hopefully this fixes the issue with characters being unable to be printed separately.

--- functions/print-pretty.js
+++ functions/print-pretty.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const MongoClient = require('mongodb').MongoClient;
 const capitalize = require('./capitalize');
 const checkPriv = require('./priv-check');
@@ -28,10 +28,10 @@ function getMap(name) {
   return map[name]();
 }
 
-function getPage(name) {
+function getPage(message, name) {
 
     let sheet = '';
-    let character = global_character;
+    let character = global_character[message.id];
     let response = getMap(name)
         .setTimestamp();
 
@@ -42,7 +42,7 @@ function getPage(name) {
                 "Refresh: " + character.refresh + "\n" +
                 "Fate Points: " + character.fate_points + "\n";
             response
-                .setTitle(global_character.character_name.capitalize())
+                .setTitle(character.character_name.capitalize())
                 .setDescription(sheet);
             sheet = '';
             let approachlist = ['', '', '', '', '', ''];
@@ -102,7 +102,7 @@ function getPage(name) {
                 sheet += '\n'
             }
             response
-                .setTitle(global_character.character_name.capitalize() + ': Aspects')
+                .setTitle(character.character_name.capitalize() + ': Aspects')
                 .setDescription(sheet);
             break;
         case 'stunts':
@@ -117,12 +117,12 @@ function getPage(name) {
                 sheet += '\n';
             }
             response
-                .setTitle(global_character.character_name.capitalize() + ': Stunts')
+                .setTitle(character.character_name.capitalize() + ': Stunts')
                 .setDescription(sheet);
             break;
         case 'conditions':
             response
-                .setTitle(global_character.character_name.capitalize() + ': Stress and Conditions')
+                .setTitle(character.character_name.capitalize() + ': Stress and Conditions')
             sheet = ''
             for (let stress in character.stress) {
                 sheet += stress.capitalize() + " : "
@@ -159,7 +159,7 @@ function getPage(name) {
             break;
         case 'image':
             response
-                .setTitle(global_character.character_name.capitalize() + ': Image')
+                .setTitle(character.character_name.capitalize() + ': Image')
             try {
                 response
                     .setImage(character.imgurl);
@@ -181,24 +181,24 @@ const emojiList = ['🏠', '🇦', '🇸', '🇨', '📷'];
 function onCollect(emoji, message, i, getPage) {
     switch (emoji.name){
         case '🏠':
-            message.edit(getPage('home'));
+            message.edit(getPage(message, 'home'));
             break;
         case '🇦':
-            message.edit(getPage('aspects'));
+            message.edit(getPage(message, 'aspects'));
             break;
         case '🇸':
-            message.edit(getPage('stunts'));
+            message.edit(getPage(message, 'stunts'));
             break;
         case '🇨':
-            message.edit(getPage('conditions'));
+            message.edit(getPage(message, 'conditions'));
             break;
         case '📷':
-            message.edit(getPage('image'));
+            message.edit(getPage(message, 'image'));
             break;
     }
 }
 
-const time = 60*60000;
+const time = 2*60000;
 
 function createCollectorMessage(message, getPage) {
   let i = 0;
@@ -206,17 +206,32 @@ function createCollectorMessage(message, getPage) {
   collector.on('collect', r => {
     i = onCollect(r.emoji, message, i, getPage);
   });
-  collector.on('end', collected => message.clearReactions());
+  collector.on('end', collected => endMessage(message));
+  return message;
 }
 
-function sendSheet(message, page){
-    message.say(getPage(page))
+function endMessage(message) {
+    let collected = message.clearReactions();
+    delete global_character[message.id];
+    console.log(global_character);
+    return collected;
+}
+
+function sheetFix(printID, newID) {
+    global_character[newID] = global_character[printID];
+    delete global_character[printID];
+    return;
+}
+
+function sendSheet(message, print_message, page){
+    message.say(getPage(print_message, page))
         .then(msg => msg.react(emojiList[0]))
         .then(msgReaction => msgReaction.message.react(emojiList[1]))
         .then(msgReaction => msgReaction.message.react(emojiList[2]))
         .then(msgReaction => msgReaction.message.react(emojiList[3]))
         .then(msgReaction => msgReaction.message.react(emojiList[4]))
-        .then(msgReaction => createCollectorMessage(msgReaction.message, getPage));
+        .then(msgReaction => createCollectorMessage(msgReaction.message, getPage))
+        .then(msg => sheetFix(print_message.id, msg.id));
 }
 
 module.exports = function printPretty(message, userid, scope, reason) {
@@ -248,26 +263,26 @@ module.exports = function printPretty(message, userid, scope, reason) {
                             message.reply("that character doesn't exist.");
                             reject(err)
                         } else {
-                            global_character = character;
+                            global_character[message.id] = character;
 
                             if ((scope == "base" || scope == "approaches") && reason == 'view') {
-                                sendSheet(message, 'home');
+                                sendSheet(message, message, 'home');
                             } else if (scope == "aspects" && reason == 'view') {
-                                sendSheet(message, 'aspects');
+                                sendSheet(message, message, 'aspects');
                             } else if (scope == "stunts" && reason == 'view') {
-                                sendSheet(message, 'stunts');
+                                sendSheet(message, message, 'stunts');
                             } else if ((scope == "condition" || scope == "stress") && reason == 'view') {
-                                sendSheet(message, 'conditions');
+                                sendSheet(message, message, 'conditions');
                             } else if (scope == "base" || scope == "approaches") {
-                                message.say(getPage('home'));
+                                message.say(getPage(message, 'home'));
                             } else if (scope == "aspects") {
-                                message.say(getPage('aspects'));
+                                message.say(getPage(message, 'aspects'));
                             } else if (scope == "stunts") {
-                                message.say(getPage('stunts'));
+                                message.say(getPage(message, 'stunts'));
                             } else if (scope == "condition" || scope == "stress") {
-                                message.say(getPage('conditions'));
+                                message.say(getPage(message,'conditions'));
                             } else {
-                                sendSheet(message, 'home');
+                                sendSheet(message, message, 'home');
                             }
                             if (reason == 'edit') {
                                 let gmrole = message.guild.roles.find(role => role.name === "GM");
